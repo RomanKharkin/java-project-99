445ad31aabbb154bf2291bd64cc4dcb2f75e00b2
Roman Kharkin <rv.h@bk.ru>
2024-01-10 19:31:10 +0100

    Fixx dockkerfiile

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY /app .
 
-RUN gradle installDist
+RUN ./gradlew --no-daemon dependencies
 
-CMD ./build/install/app/bin/app
+RUN ./gradlew --no-daemon build
+
+CMD java -jar build/libs/app-0.0.1-SNAPSHOT.jar
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
 	application
 	id("io.freefair.lombok") version "8.4"
-	id("org.springframework.boot") version "3.2.2-SNAPSHOT"
+	id("org.springframework.boot") version "3.2.0"
 	id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "hexlet.code"
+group = "hexlet.code.app"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -30,28 +30,19 @@ dependencies {
 	implementation("net.datafaker:datafaker:2.0.2")
 	implementation("org.instancio:instancio-junit:3.3.1")
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.postgresql:postgresql:42.6.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation(platform("org.junit:junit-bom:5.10.0"))
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
 
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-	testImplementation("org.springframework.security:spring-security-test")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+//	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-application { mainClass.set("hexlet.code.App") }
-
-tasks.jar {
-	manifest {
-		attributes(
-				mapOf(
-						"Main-Class" to "hexlet.code.App"
-				)
-		)
-	}
-}
+application { mainClass.set("hexlet.code.app.AppApplication") }
