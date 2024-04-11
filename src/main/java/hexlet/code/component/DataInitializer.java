c873f57727a83b1d81e7feb9c0454b83dd5481f1
Roman Kharkin <rv.h@bk.ru>
2024-04-11 17:39:58 +0200

    Fix Errors N5

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -49,9 +49,13 @@ public class DataInitializer implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         userRepository.deleteAll();
+        userRepository.flush();
         taskStatusRepository.deleteAll();
+        taskStatusRepository.flush();
         taskRepository.deleteAll();
+        taskRepository.flush();
         labelRepository.deleteAll();
+        labelRepository.flush();
 
         var userData = new UserCreateDTO();
         userData.setEmail("hexlet@example.com");
